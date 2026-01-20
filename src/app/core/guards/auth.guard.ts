1f379a782db9bef3985dc7d43a585ec980c73bd9
mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2026-01-20 12:13:44 +0200

    solvey

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,12 +1,11 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const _Router = inject(Router);
-  if (
-    typeof localStorage !== 'undefined' &&
-    localStorage.getItem('userToken')
-  ) {
+  const _PLATFORM_ID = inject(PLATFORM_ID);
+  if (isPlatformBrowser(_PLATFORM_ID) && localStorage.getItem('userToken')) {
     return true;
   }
   _Router.navigate(['/login']);
