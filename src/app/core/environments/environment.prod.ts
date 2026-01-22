b7ef32481098f4501f9ae539f34068f127c20661
mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2026-01-22 14:00:39 +0200

    solve allorders problem-3

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -3,5 +3,4 @@ export const environment = {
   // Keep the API URL the same
   apiUrl: 'https://ecommerce.routemisr.com/api/v1/',
   // CHANGE THIS: Use your actual Vercel link here
-  baseUrl: '?url=https://angular-ecommerce-murex.vercel.app/',
 };
--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -8,14 +8,16 @@ import { environment } from '../environments/environment';
 })
 export class OrderService {
   constructor(private _HttpClient: HttpClient) {}
+  
   checkOutSession(id: any, userInfo: object): Observable<any> {
     // window.location.origin >> بتجيب رابط الموقع الحالي أوتوماتيك
-    // مثال: https://your-app.vercel.app
+  // مثال: https://your-app.vercel.app
     return this._HttpClient.post(
-      `${environment.apiUrl}orders/checkout-session/${id}?url=${window.location.origin}/%23`,
+      `${environment.apiUrl}orders/checkout-session/${id}?url=${window.location.origin}`,
       {
         shippingAddress: userInfo,
       },
     );
+    
   }
 }
